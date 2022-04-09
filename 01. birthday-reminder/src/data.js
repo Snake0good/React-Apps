70586743f0274b87dbac00b92904615396bce08e
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-04-09 18:14:53 +0800

    Add files via upload

---
+++ 01. birthday-reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Barbara Terrence',
+      age: 29,
+      image:
+        'https://images.unsplash.com/photo-1611432579699-484f7990b127?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    },
+    {
+      id: 2,
+      name: 'Wynona Gordon',
+      age: 32,
+      image:
+        'https://images.unsplash.com/photo-1627161683077-e34782c24d81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    },
+    {
+      id: 3,
+      name: 'Khamil Konderan',
+      age: 36,
+      image:
+        'https://images.unsplash.com/photo-1595211877493-41a4e5f236b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    },
+    {
+      id: 4,
+      name: 'John Walsh',
+      age: 34,
+      image:
+        'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    },
+    {
+      id: 5,
+      name: 'Leatrice Callan',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+];
