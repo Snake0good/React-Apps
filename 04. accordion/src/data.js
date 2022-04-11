66813f35dfaef1d43b5ab13ad8db929613a29dd3
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-04-11 21:58:30 +0800

    Add files via upload

---
+++ 04. accordion/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+      id: 1,
+      title: "What's the difference between a poorly dressed man on a tricycle and a well dressed man on a bicycle?",
+      info:
+        'Attire',
+    },
+    {
+      id: 2,
+      title: 'Did you hear about the cheese factory that exploded in France?',
+      info:
+        'There was nothing left but de Brie',
+    },
+    {
+      id: 3,
+      title: 'What does the cheese say after making a jump shot?',
+      info:
+        'Swisssss',
+    },
+    {
+      id: 4,
+      title: 'Do you know where you can get chicken broth in bulk?',
+      info:
+        'The stock market',
+    },
+    {
+      id: 5,
+      title: 'What did the excited gardener do when Spring finally came?',
+      info:
+        'Wet his plants',
+    },
+  ]
+  export default questions
