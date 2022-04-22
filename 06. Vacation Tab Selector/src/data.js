e2fe5ee0587d882f94d1d3e9b953b52762804cc2
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-04-22 12:51:04 +0800

    Add files via upload

---
+++ 06. Vacation Tab Selector/src/data.js
@@ -0,0 +1,51 @@
+const menu = [
+    {
+      id: '1',
+      title: 'package one',
+      price: "1295  -  3 nights / 2 days",
+      img: 'https://images.unsplash.com/photo-1605216663980-b7ca6e9f2451?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1014&q=80',
+      desc: `A beautiful getaway to the island in the south`,
+      extra: [
+        "Snorkeling off the reef. You'll be guided with an expert guid who has lived their life in the sea. Fish, sea turtle, dolphins, and whales if you're lucky", 
+        "One day you’re watching giant tortoises mate in swirling mists, then you’re nose-to-nose with a seafaring marine iguana, or snorkelling with a group of penguins.  ", 
+        "As Darwin appreciated, neighbouring islands have sub-species that have developed differently, leading to one of the most important, world-changing discoveries ever known: evolution."
+      ]
+    },
+    {
+      id: 2,
+      title: 'package two',
+      price: "1895  -  4 nights / 3 days",
+      img: 'https://images.unsplash.com/photo-1617339860293-8c1b58e7c4ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+      desc: `beach bums and market bazaars`,
+      extra: [
+        "Wanna check out some hand crafted goods and spend your days relaxing on the beach? This is what you're after then!", 
+        "Tortuguero means place of turtles in Spanish, so head to Tortuguero NP on the north of the Caribbean coast. Visit a beach at night and you might spot hawksbill (Mar-Nov)", 
+        "If you want to see olive ridley, visit Santa Rosa NP on the Pacific coast (Jul-Feb). Meanwhile, you can see Hoffman’s tow-toed sloth and brown-throated sloth on Osa Peninsula."
+      ]
+    },
+    {
+      id: 3,
+      title: 'package three',
+      price: "2195  -  5 nights / 4 days",
+      img: 'https://images.unsplash.com/photo-1509233725247-49e657c54213?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=898&q=80',
+      desc: `major relaxing and cocunut smoothies`,
+      extra: [
+        "Prop up a beach chair and get your smiling on. If you want shade, you got it. If you want sunshine, you got it. You want peace? This is for you!", 
+        "There are some small Mayan vestiges of the pre-Columbian Maya civilization in Cancún. El Rey (Las Ruinas del Rey) is located in the Hotel Zone.", 
+        "El Meco, a more substantial site, is found on the mainland just outside the city limits on the road north to Punta Sam."
+      ]
+    },
+    {
+      id: 4,
+      title: 'package four',
+      price: "2895  -  7 nights / 6 days",
+      img: 'https://images.unsplash.com/photo-1533991310801-340302384c22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+      desc: `Sunset lover and grill master + ALL`,
+      extra: [
+        "There is something for everyone here. Include previous packages PLUS a night fishing session. You're guaranteed to catch some fish, and we'll BBQ it up for you minutes after it leaves the sea!", 
+        "Despite being a young city, Cancún has a memorial monument of its foundation on a replica of the old Airport Control Tower. ", 
+        "The original control tower was a provisional wooden structure, the work of Mexican architects Agustín and Enrique Landa Verdugo. "
+      ]
+    }
+  ];
+  export default menu;
