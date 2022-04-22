fc77a3ad038165109c5b8371bcec481bd9b2ce48
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-04-22 12:52:10 +0800

    Add files via upload

---
+++ 07. Favorite Animated Characters Slider/src/data.js
@@ -0,0 +1,44 @@
+const people = [
+    {
+      id: 1,
+      image:
+        'https://images.unsplash.com/photo-1589254065878-42c9da997008?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+      name: 'WALL-E',
+      title: "he's still trying to save the planet",
+      quote:
+        'WALL- E (Waste Allocation Load Lifter - Earth-Class, with the formal designation N. 62675) is a waste-collector drone created by the Buy n Large corporation initially programmed to haul the garbage plaguing Earth in the distant future...',
+      catchPhrase: "EEEEEEEVVVVVVAAAAAA!"
+    },
+    {
+      id: 2,
+      image:
+        'https://images.unsplash.com/photo-1581538398550-23030b3e2512?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+      name: 'groot',
+      title: 'guardians of the galaxy tree-man',
+      quote:
+        'Groot is a Flora colossus and the accomplice of Rocket Raccoon. Together, the pair had traveled the galaxy picking up bounties until they met Star-Lord and Gamora where they also met Drax the Destroyer. There, they agreed to work together to escape and sell the Orb for a massive profit..',
+      catchPhrase: "I am Groot."
+    },
+    {
+      id: 3,
+      image:
+        'https://images.unsplash.com/photo-1581714716878-d03216848afd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+      name: 'sheriff woody',
+      title: 'the sheriff from toy story',
+      quote:
+        "Woody served as the leader of Andy's toys, a position he upheld by looking out for each member of the group. When Andy enters adulthood and leaves for college, Woody is donated to a little girl named Bonnie Anderson, and gets a girlfriend Bo Peep",
+      catchPhrase: "There's a snake in my boots!"
+    },
+    {
+      id: 4,
+      image:
+        'https://thumbs.dreamstime.com/b/anna-character-figure-frozen-movie-event-magical-journey-poster-roadshow-promotion-new-disney-dubai-uae-december-166650743.jpg',
+      name: 'queen anna of arendelle',
+      title: 'frozen is really about how anna saves the day',
+      quote:
+        "Sure, Elsa is great too, but we gotta remember that she was the one who took Anna's powers. She almost killed her sister! And how does Anna repay her? By saving her life and FREEZING herself.",
+      catchPhrase: "For the first time in forever, nothing's in my way!"
+    },
+  ];
+  
+  export default people;
