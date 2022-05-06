6d91cbac59c253c19e218bf71eb506c4560a9a07
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-05-06 13:21:21 +0800

    Update context.js

--- 12. Sidebar/src/context.js
+++ 12. Sidebar/src/context.js
@@ -1,10 +1,12 @@
 import React, { useState, useContext } from 'react';
+import loadingImg from './images/loading.gif'
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [imgSrc, setImageSrc] = useState('');
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -14,8 +16,18 @@ const AppProvider = ({ children }) => {
   };
 
   const openModal = () => {
+    setImageSrc(loadingImg)
     setIsModalOpen(true);
+    // call an api from here 
+    fetch(`https://api.thecatapi.com/v1/images/search`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        setImageSrc(data[0].url)
+      })
+      .catch(err => console.log(err))
   };
+
   const closeModal = () => {
     setIsModalOpen(false);
   };
@@ -29,6 +41,7 @@ const AppProvider = ({ children }) => {
         closeModal,
         openSidebar,
         closeSidebar,
+        imgSrc
       }}
     >
       {children}
