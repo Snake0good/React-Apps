1712a1f7ab5312a98f102574314772e09a147ba1
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-05-06 13:21:39 +0800

    Update Modal.js

--- 12. Sidebar/src/Modal.js
+++ 12. Sidebar/src/Modal.js
@@ -1,14 +1,15 @@
 import { FaTimes } from 'react-icons/fa'
 import { useGlobalContext } from './context'
+import catPicture from './images/cat.jpg'
 
 const Modal = () => {
-    const { isModalOpen, closeModal } = useGlobalContext();
+    const { isModalOpen, closeModal, imgSrc } = useGlobalContext();
 
 
     return (
         <div className={`${ isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'}`} >
             <div className='modal-container'>
-                <h3>Modal Content</h3>
+                <img id="cat-pic" src={imgSrc} alt="cat-picture"></img>
                 <button onClick={closeModal} className='close-modal-btn'>
                     <FaTimes></FaTimes>
                 </button>
